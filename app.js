a2aef4ba00eda3001cd0425b042c1874532a4b5f
sakisalman <codersaki98@gmail.com>
2022-01-06 02:38:37 +0600

    Uploaded

---
+++ app.js
@@ -0,0 +1,34 @@
+const clock = document.querySelector('.clock-wrap');
+
+const h = clock.querySelector('#h');
+const m = clock.querySelector('#m');
+const s = clock.querySelector('#s');
+const amp = clock.querySelector('#amp');
+
+setInterval(() => {
+
+     let date = new Date();
+
+let hour = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+
+if (h > 12) {
+
+     amp.innerHTML = "PM";
+     
+}else{
+     amp.innerHTML = "AM";
+}
+
+h.innerHTML = `${hour < 10 ? "0" + hour : hour }:`;
+m.innerHTML = `${min < 10 ? "0" + min : min }:`;
+s.innerHTML = `${sec < 10 ? "0" + sec : sec }`;
+
+
+     
+}, 1000);
+
+
+
+
